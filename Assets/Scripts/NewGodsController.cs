0b013cf54a800289b73ecc4acec1fe21b90503b6
lordvlads77 <81182279+lordvlads77@users.noreply.github.com>
2021-12-01 07:34:19 -0600

    Lvl1 Finished Working on Lvl2

---
+++ Assets/Scripts/NewGodsController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class NewGodsController : MonoBehaviour
+{
+    public static GameObject sounds1;
+    public static GameObject sounds2;
+
+    public List<GameObject> BirdSoundsObj = new List<GameObject> {sounds1, sounds2};
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
